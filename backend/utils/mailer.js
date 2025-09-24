28fba88fde89bd42875d27d35522111cd6ce4250
EMMANUEL NEEQUAYE <eneequaye006@st.ug.edu.gh>
2025-09-24 11:29:04 +0000

    feat: enhance mailer with debug logging for environment variables and email process

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,4 +1,9 @@
+
 const nodemailer = require('nodemailer');
+
+// Debug: Log environment variables
+console.log('GMAIL_USER:', process.env.GMAIL_USER);
+console.log('GMAIL_PASS:', process.env.GMAIL_PASS ? '[HIDDEN]' : 'undefined');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -9,12 +14,21 @@ const transporter = nodemailer.createTransport({
 });
 
 function sendMail({ to, subject, text, html }) {
+  console.log('Sending email to:', to);
   return transporter.sendMail({
     from: process.env.GMAIL_USER,
     to,
     subject,
     text,
     html
+  })
+  .then(info => {
+    console.log('Email sent:', info.response);
+    return info;
+  })
+  .catch(error => {
+    console.error('Email send error:', error);
+    throw error;
   });
 }
 
